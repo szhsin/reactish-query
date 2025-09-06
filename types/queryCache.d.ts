db5e558ab3237db8b4635011f4b6b582549c6fd0
Zheng Song <szhsin9@gmail.com>
2025-09-06 20:06:15 +1000

    chore: add type parameter to cache function

--- types/queryCache.d.ts
+++ types/queryCache.d.ts
@@ -1,7 +1,7 @@
-type QueryCache = {
+type QueryCache<TValue extends object = object> = {
     clear: () => void;
-    get: (key: string, strong?: boolean) => object | undefined;
-    set: (key: string, value: object, strong?: boolean) => void;
+    get: (key: string, strong?: boolean) => TValue | undefined;
+    set: (key: string, value: TValue, strong?: boolean) => void;
 };
-declare const createQueryCache: () => QueryCache;
+declare const createQueryCache: <TValue extends object>() => QueryCache<TValue>;
 export { createQueryCache, type QueryCache };
