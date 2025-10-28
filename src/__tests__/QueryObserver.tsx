3dd11b9a06062422b5d1d6b55dc200ffd0720e27
Zheng Song <szhsin9@gmail.com>
2025-10-28 22:22:25 +1100

    test: add tests for useQueryObserver event metadata

--- src/__tests__/QueryObserver.tsx
+++ src/__tests__/QueryObserver.tsx
@@ -1,15 +1,35 @@
 import { type ReactNode, useState } from 'react';
-import type { QueryHookOptions, QueryObserverOptions } from '../index';
-import { useQuery } from '../index';
+import type {
+  QueryObserverOptions,
+  QueryHookOptions,
+  LazyQueryHookOptions,
+  MutationHookOptions,
+  ObserverMeta
+} from '../index';
+import { useQuery, useLazyQuery$, useMutation } from '../index';
 import { useQueryObserver } from '../composable';
 import { fakeRequest } from './fakeRequest';
 
-const useQueryWithObserver = <TData, TKey = unknown>({
+export const useQueryWithObserver = <TData, TKey = unknown>({
   onData,
   onError,
   ...options
 }: QueryHookOptions<TData, TKey> & QueryObserverOptions<TData, TKey>) =>
   useQueryObserver(useQuery(options), { onData, onError });
+
+export const useLazyQueryWithObserver$ = <TData, TArgs, TKey = unknown>({
+  onData,
+  onError,
+  ...options
+}: LazyQueryHookOptions<TData, TArgs, TKey> & QueryObserverOptions<TData, TKey, TArgs>) =>
+  useQueryObserver(useLazyQuery$(options), { onData, onError });
+
+export const useMutationWithObserver = <TData, TArgs, TKey = unknown>({
+  onData,
+  onError,
+  ...options
+}: MutationHookOptions<TData, TArgs, TKey> & QueryObserverOptions<TData, TKey, TArgs>) =>
+  useQueryObserver(useMutation(options), { onData, onError });
 
 const QueryObserver = ({
   queryName,
@@ -25,8 +45,14 @@ const QueryObserver = ({
   noFetcher?: boolean;
   children?: ReactNode;
 } & Pick<QueryHookOptions<unknown, unknown>, 'cacheMode' | 'enabled' | 'staleTime'> & {
-    onData?: (args: { data: number; id: number; query: string }) => void;
-    onError?: (args: { error: string; id: number; query: string }) => void;
+    onData?: (
+      args: { data: number; id: number; query: string },
+      metadata: ObserverMeta<{ requestId: number }>
+    ) => void;
+    onError?: (
+      args: { error: string; id: number; query: string },
+      metadata: ObserverMeta<{ requestId: number }>
+    ) => void;
   }) => {
   const [id, setId] = useState(defaultId);
   const { isFetching, error, data, refetch } = useQueryWithObserver({
@@ -37,8 +63,9 @@ const QueryObserver = ({
       queryFn: (arg: { queryKey: { requestId: number } }) =>
         fakeRequest((arg.queryKey.requestId + id) / 2)
     }),
-    onData: (data) => onData?.({ data, id, query: queryName }),
-    onError: (error) => onError?.({ error: error.message, id, query: queryName })
+    onData: (data, metadata) => onData?.({ data, id, query: queryName }, metadata),
+    onError: (error, metadata) =>
+      onError?.({ error: error.message, id, query: queryName }, metadata)
   });
 
   return (
@@ -61,4 +88,63 @@ const QueryObserver = ({
   );
 };
 
-export { QueryObserver };
+const MutationObserver = ({
+  queryName,
+  defaultId = 1,
+  children,
+  onData,
+  onError
+}: {
+  queryName: string;
+  defaultId?: number;
+  children?: ReactNode;
+} & {
+  onData?: (
+    args: { data: number; id: number; query: string },
+    metadata: ObserverMeta<{ keyId: number }, { paramId: number }>
+  ) => void;
+  onError?: (
+    args: { error: string; id: number; query: string },
+    metadata: ObserverMeta<{ keyId: number }, { paramId: number }>
+  ) => void;
+}) => {
+  const [id, setId] = useState(defaultId);
+  const { isFetching, error, data, args, trigger } = useMutationWithObserver<
+    number,
+    { paramId: number },
+    { keyId: number }
+  >({
+    queryKey: { keyId: id },
+    // testing queryFn use both local and variables from the arguments
+    queryFn: ({ queryKey, args }) =>
+      fakeRequest((id + queryKey!.keyId + args.paramId) / 3),
+    onData: (data, metadata) => onData?.({ data, id, query: queryName }, metadata),
+    onError: (error, metadata) =>
+      onError?.({ error: error.message, id, query: queryName }, metadata)
+  });
+
+  return (
+    <section>
+      <div data-testid={`query-${queryName}`}>Query {queryName}</div>
+      <div data-testid={`status-${queryName}`}>{isFetching ? 'fetching' : 'idle'}</div>
+      <div data-testid={`error-${queryName}`}>{error?.message}</div>
+      <div data-testid={`data-${queryName}`}>{data}</div>
+      <div data-testid={`args-${queryName}`}>{args?.paramId}</div>
+      <button data-testid={`plus-${queryName}`} onClick={() => setId((s) => s + 1)}>
+        Plus
+      </button>
+      <button data-testid={`minus-${queryName}`} onClick={() => setId((s) => s - 1)}>
+        Minus
+      </button>
+      <button
+        data-testid={`trigger-${queryName}`}
+        onClick={() => trigger({ paramId: id })}
+      >
+        Trigger
+      </button>
+      {children}
+    </section>
+  );
+};
+
+export { QueryObserver, MutationObserver };
