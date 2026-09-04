76b5a5f385e9c3cb3b4d87affb517e31678afc42
szhsin <41896553+szhsin@users.noreply.github.com>
2026-09-04 18:44:00 +0000

    chore(deps): update all dependencies

--- dist/cjs/middleware/index.cjs
+++
@@ -1,5 +0,0 @@
-"use client";
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-const require_queryObserver = require("./queryObserver.cjs");
-exports.queryObserver = require_queryObserver.queryObserver;
--- dist/esm/QueryContext.mjs
+++
@@ -1,9 +0,0 @@
-import { defaultQueryClient } from "./queryClient.mjs";
-import { createContext } from "react";
-//#region src/QueryContext.ts
-const QueryContext = createContext({
-	client: defaultQueryClient,
-	defaultOptions: {}
-});
-//#endregion
-export { QueryContext };
